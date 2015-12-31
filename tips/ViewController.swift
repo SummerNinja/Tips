c41a87f2fe2b2a66b52f34afa893b14f0098da01
Melanie L. Mendoza <mendom16@d-172-28-35-79.dhcp4.washington.edu>
2015-12-31 15:24:28 -0800

    Version 4

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billField.becomeFirstResponder()
+        
         let userLocale = NSLocale.currentLocale()
         let currencySymbol = userLocale.objectForKey(NSLocaleCurrencySymbol) as! String
         print(currencySymbol)
